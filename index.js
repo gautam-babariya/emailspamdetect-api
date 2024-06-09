13fa3f97c48a2e5929794dc46416cb3470ed4d6a
gautam <gautambabariya0125@gmail.com>
2024-06-09 13:06:03 +0530

    back change

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const cors = require('cors');
 
 // cors code 
 app.use(cors())
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-    res.setHeader('Access-Control-Max-Age', 2592000);
-    next();
-});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
